5fd785832b28344299fc0a823331486bfe07d907
Steven He <hez2010@outlook.com>
2018-09-08 10:15:44 +0800

    添加账户额外信息

---
+++ Data/Migrations/20180908021330_AddAdditionalInfo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mscfreshman.Data.Migrations
+{
+    public partial class AddAdditionalInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AdditionalInfo",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdditionalInfo",
+                table: "AspNetUsers");
+        }
+    }
+}
