722a616929581aa8b81acb937bd325cd70c5276c
Steven He <hez2010@outlook.com>
2018-09-13 22:44:47 +0800

    其他信息模板更新

--- Data/Identity/OtherInfoTemplate.cs
+++ Data/Identity/OtherInfoTemplate.cs
@@ -4,14 +4,17 @@ namespace mscfreshman.Data.Identity
 {
     public class OtherInfoTemplate
     {
-        [Name("自我介绍")]
+        [Name("自我介绍、个人经历")]
         public string introduction { get; set; }
 
-        [Name("自身优点")]
+        [Name("自身优点、爱好特长")]
         public string advantages { get; set; }
 
-        [Name("对 MSC 的期待")]
+        [Name("加入的理由、对 MSC 的期待")]
         public string wishes { get; set; }
+
+        [Name("还想参加的其他部门")]
+        public string otherdepartment { get; set; }
 
     }
 
